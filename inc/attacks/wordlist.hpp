5db6aeaa339df25bb4f1dd16b67f888a7d58b65d
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-06-19 00:18:18 -0600

    Implement the structure for the wordlist scanner

--- inc/attacks/wordlist.hpp
+++ inc/attacks/wordlist.hpp
@@ -3,7 +3,10 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 std::vector<std::string> parse_candidate_file(const std::string& file_path);
+std::string wordlist_scan(const std::unordered_map<std::string, std::string>& password_db,  std::string& candidate_file, std::string target, bool multithread = false);
+
 
 #endif
--- src/attacks/wordlist.cpp
+++ src/attacks/wordlist.cpp
@@ -17,3 +17,30 @@ std::vector<std::string> parse_candidate_file(const std::string& file_path){
     candidate_file.close();
     return candidates;
 }
+
+std::string scan_singlethread(const std::string checksum, const std::vector<std::string>& candidates){
+    std::string result = "";
+    return result;
+}
+
+std::string scan_multithread(const std::string checksum, const std::vector<std::string>& candidates){
+    std::string result = "";
+    return result;
+}
+
+// performs a scan of a password database, returns the password fro the provided user if found, otherwise returns an emptty string
+std::string wordlist_scan(const std::unordered_map<std::string, std::string>& password_db,  const std::string& candidate_file, const std::string& target, bool multithread = false){
+    // ensure there is a checksum for the provdied username
+    auto tmp = password_db.find(target);
+    if (tmp == password_db.end())
+        throw std::runtime_error("User " + target + " not found.");
+    std::string checksum  = tmp->second;
+    // open the candidate file
+    std::vector<std::string> candidates = parse_candidate_file(candidate_file);
+    // run the scan
+    if (multithread)
+        return scan_multithread(checksum, candidates);
+    else 
+        return scan_singlethread(checksum, candidates);
+
+}
